508cb8bf81f10904bbc3da772579b0ad816dcfed
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2021-12-30 13:07:42 +0500

    4th commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./Layout/Header/Header.js";
 import Hero from "./Components/Hero/Hero.js";
 import About from "./Components/About/About.js";
+import Team from "./Components/Team/Team.js";
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
       <About />
+      <Team />
     </div>
   );
 }
---
+++ src/Components/Team/Team.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Team.css";
+import img1 from "../../Assets/Team/test1-no-bg.png";
+import img2 from "../../Assets/Team/test2-no-bg.png";
+import img3 from "../../Assets/Team/test3-no-bg.png";
+import img4 from "../../Assets/Team/test4-no-bg.png";
+import img5 from "../../Assets/Team/test5-no-bg.png";
+import img6 from "../../Assets/Team/test6-no-bg.png";
+import img7 from "../../Assets/Team/test7-no-bg.png";
+function Team() {
+  return (
+    <div className="team">
+      <h1>OUR TEAM</h1>
+      <div className="team-grid">
+        <img className="team_image" src={img1} alt="..." />
+        <img className="team_image" src={img2} alt="..." />
+        <img className="team_image" src={img3} alt="..." />
+        <img className="team_image" src={img4} alt="..." />
+        <img className="team_image" src={img5} alt="..." />
+        <img className="team_image" src={img6} alt="..." />
+        <img className="team_image" src={img7} alt="..." />
+        <p className="see-more">
+          SEE <br></br> MORE
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Team;
