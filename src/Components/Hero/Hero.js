fe477e0add6137e443c8bb96e40a5456bc48e917
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-01-02 19:12:30 +0500

    finalised14

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./Hero.css";
 //assets
 import img from "../../Assets/Images/test.png";
@@ -9,6 +9,41 @@ import img5 from "../../Assets/Images/test5.png";
 import img6 from "../../Assets/Images/test6.png";
 import img7 from "../../Assets/Images/test7.png";
 function Hero() {
+  const [showTimer, setShowTimer] = useState(false);
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+  let interval = useRef();
+  const startTimer = () => {
+    const countdownDate = new Date("Jan 21, 2022 00:00:00").getTime();
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      if (distance < 0) {
+        // stop our timer
+        clearInterval(interval.current);
+      } else {
+        // update timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    });
+  };
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
   return (
     <div id="hero" className="hero">
       <h1 data-aos="fade-up">Collect Beans On the Metaverse</h1>
@@ -16,9 +51,35 @@ function Hero() {
         5000 confused beans who have somehow made their way to the metaverse.
         Only on Solana
       </p>
-      <button data-aos="fade-up" data-aos-delay="200" className="hero_connect">
-        Connect
-      </button>
+      {!showTimer ? (
+        <button
+          onClick={() => setShowTimer(true)}
+          data-aos="fade-up"
+          data-aos-delay="200"
+          className="hero_connect"
+        >
+          Connect
+        </button>
+      ) : (
+        <div className="timer-div">
+          <div className="time-box">
+            <h2>Days</h2>
+            <h2>{timerDays}</h2>
+          </div>
+          <div className="time-box">
+            <h2>Hours</h2>
+            <h2>{timerHours}</h2>
+          </div>
+          <div className="time-box">
+            <h2>Minutes</h2>
+            <h2>{timerMinutes}</h2>
+          </div>
+          <div className="time-box">
+            <h2>Seconds</h2>
+            <h2>{timerSeconds}</h2>
+          </div>
+        </div>
+      )}
 
       <div
         data-aos="fade-up"
