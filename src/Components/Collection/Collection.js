6dbe49fcc56626642e05fa889bcd9486d2aa7a56
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-01-08 13:58:55 +0500

    final22

--- src/Components/Collection/Collection.js
+++ src/Components/Collection/Collection.js
@@ -34,7 +34,7 @@ function Collection() {
         <div className="collect-div number1">
           <div className="collect_left">
             <h1>Fair Launch</h1>
-            <p>
+            <p className="hover-p">
               Each waifu NFT is 0.03/0.05/0.07 ETH. Only 5/4/3 can be minted per
               transaction. No bonding curve.
             </p>
@@ -46,7 +46,7 @@ function Collection() {
         <div className="collect-div number2">
           <div className="collect_left">
             <h1>Win Prizes</h1>
-            <p>
+            <p className="hover-p">
               Win Prizes Each waifu NFT is a ticket to participate in every
               lottery event. Each ticket grants X number of entries based on the
               color rarity.
@@ -59,7 +59,7 @@ function Collection() {
         <div className="collect-div number3">
           <div className="collect_left">
             <h1>Limited</h1>
-            <p>
+            <p className="hover-p">
               Only a total of 8,888 waifus will ever be minted into existence,
               making them super rare. 6 color rarities gives you a chance to
               mint an extra rare one!
@@ -72,7 +72,7 @@ function Collection() {
         <div className="collect-div number4">
           <div className="collect_left">
             <h1>Make History</h1>
-            <p>
+            <p className="hover-p">
               All Waifustation NFT owners will forever be engraved in history
               via Waifustation OVA.
             </p>
@@ -84,7 +84,7 @@ function Collection() {
         <div className="collect-div number5">
           <div className="collect_left">
             <h1>Unique Art</h1>
-            <p>
+            <p className="hover-p">
               Each NFT waifu is a uniquely composed 1 of 1 piece of artwork
               drawn by our styleGAN2 deep learning AI overlord. No two waifus
               are the same!
@@ -98,7 +98,7 @@ function Collection() {
         <div className="collect-div number6">
           <div className="collect_left">
             <h1>Good Cause</h1>
-            <p>
+            <p className="hover-p">
               Upon 80% completion of the Mission Roadmap, 10 ETH will be donated
               to Kyoto Animation.
             </p>
@@ -110,7 +110,7 @@ function Collection() {
         <div className="collect-div number7">
           <div className="collect_left">
             <h1>Ownership</h1>
-            <p>
+            <p className="hover-p">
               All waifu NFTs come with full commercial usage rights. Do as you
               please with your waifus!
             </p>
@@ -122,7 +122,7 @@ function Collection() {
         <div className="collect-div number8">
           <div className="collect_left">
             <h1>Royalties</h1>
-            <p>
+            <p className="hover-p">
               50% of ETH obtained from all gachapon machines will be
               continuously airdropped to the NFT owners.
             </p>
